1884a62aaa3396d858ff7a91900bcf9242545740
Экзамен <aiysfduagsf>
2023-03-15 18:26:27 +0300

    добавил недели

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 def convert(seconds):
+    weeks = seconds // (7 * (24 * 3600))
+    seconds %= 7 * 24
     days = seconds // (24 * 3600)
-    seconds %= 24* 3600
+    seconds %= 24 * 3600
     hours = seconds // 3600
     seconds %= 3600
     minutes = seconds // 60
     seconds %=60
-    print(f'{days}:{hours}:{minutes}:{seconds}')
+    print(f'{weeks}:{days}:{hours}:{minutes}:{seconds}')
 
 sec = int(input('введите кол-во секунд'))
 convert(sec)
---
+++ mathOpers.py
@@ -0,0 +1,7 @@
+def perimetr(dlina, shirina):
+    return 2 * (dlina + shirina)
+
+
+a = int(input())
+b = int(input())
+print(perimetr(a, b))
